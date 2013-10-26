e671a03a6e4a0632390e9369334a65ab1b9fe5c9
Michael Contento <michaelcontento@gmail.com>
2013-10-27 01:54:07 +0200

    improve log output with indent and stripped paths

--- cocowizard/utils/log.py
+++ cocowizard/utils/log.py
@@ -9,24 +9,46 @@ import sys
 import os
 import traceback
 from colorama import Fore, Back, Style
+from path import path
+from contextlib import contextmanager
+
+CURRENT_DIR = path("./").realpath()
+INDENT_LVL = 0
+INDENT_STR = ""
 
 def error(message, exit=True, end="\n"):
-    print(Fore.RED + "[ERROR]", message, Style.RESET_ALL, end=end)
+    _print(Fore.RED + "[ERROR] " + INDENT_STR + message + Style.RESET_ALL, end=end)
     if exit:
         sys.exit()
 
 def warning(message, end="\n"):
-    print(Fore.YELLOW + "[WARN]", message, Style.RESET_ALL, end=end)
+    _print(Fore.YELLOW + "[WARN] " + INDENT_STR + message + Style.RESET_ALL, end=end)
 
 def info(message, end="\n"):
-    print("[INFO]", message, end=end)
+    _print("[INFO] " + INDENT_STR + message, end=end)
 
 def debug(message, end="\n"):
     if "DEBUG" not in os.environ:
         return
 
     if isinstance(message, BaseException):
-        print(Back.RED + Fore.BLACK + "[EXCEPTION]", message, Style.RESET_ALL)
+        _print(Back.RED + Fore.BLACK + "[EXCEPTION] " + INDENT_STR + message, Style.RESET_ALL)
         traceback.print_exc()
     else:
-        print(Fore.GREEN + "[DEBUG]", message, Style.RESET_ALL, end=end)
+        _print(Fore.GREEN + "[DEBUG] " + INDENT_STR + message + Style.RESET_ALL, end=end)
+
+@contextmanager
+def indent():
+    global INDENT_LVL
+    global INDENT_STR
+
+    INDENT_LVL += 2
+    INDENT_STR += " " * INDENT_LVL
+
+    yield
+
+    INDENT_LVL -= 2
+    INDENT_STR = " " * INDENT_LVL
+
+def _print(message, end):
+    print(message.replace(CURRENT_DIR, "."), end=end)
