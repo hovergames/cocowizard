a56e6217a88a609fd9abbf680f42e0755a5e55ac
Michael Contento <michaelcontento@gmail.com>
2013-10-27 02:36:53 +0200

    refactoring inside log

--- cocowizard/utils/log.py
+++ cocowizard/utils/log.py
@@ -17,25 +17,25 @@ INDENT_LVL = 0
 INDENT_STR = ""
 
 def error(message, exit=True, end="\n"):
-    _print(Fore.RED + "[ERROR] " + INDENT_STR + message + Style.RESET_ALL, end=end)
+    _print(Fore.RED + "[ERROR]", message, end=end)
     if exit:
         sys.exit()
 
 def warning(message, end="\n"):
-    _print(Fore.YELLOW + "[WARN ] " + INDENT_STR + message + Style.RESET_ALL, end=end)
+    _print(Fore.YELLOW + "[WARN ]", message, end=end)
 
 def info(message, end="\n"):
-    _print("[INFO ] " + INDENT_STR + message, end=end)
+    _print("[INFO ]", message, end=end)
 
 def debug(message, end="\n"):
     if "DEBUG" not in os.environ:
         return
 
     if isinstance(message, BaseException):
-        _print(Back.RED + Fore.BLACK + "[EXCEPTION] " + INDENT_STR + message, Style.RESET_ALL)
+        _print(Back.RED + Fore.BLACK + "[EXCEPTION]", message, "\n")
         traceback.print_exc()
     else:
-        _print(Fore.GREEN + "[DEBUG] " + INDENT_STR + message + Style.RESET_ALL, end=end)
+        _print(Fore.GREEN + "[DEBUG]", message, end=end)
 
 @contextmanager
 def indent():
@@ -50,5 +50,8 @@ def indent():
     INDENT_LVL -= 2
     INDENT_STR = " " * INDENT_LVL
 
-def _print(message, end):
-    print(message.replace(CURRENT_DIR, "."), end=end)
+def _print(prefix, message, end):
+    if isinstance(message, str):
+        message = message.replace(CURRENT_DIR, ".")
+
+    print(prefix + INDENT_STR, message, Style.RESET_ALL, end=end)
