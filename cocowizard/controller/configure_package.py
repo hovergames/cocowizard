ff1cbd25aad4cc91eeec1d9d5a2eec322dc21245
Michael Contento <michaelcontento@gmail.com>
2013-10-28 16:57:37 +0100

    small refactorin

--- cocowizard/controller/configure_package.py
+++ cocowizard/controller/configure_package.py
@@ -15,52 +15,46 @@ XCODE_FILE = config.get("general.project") + ".xcodeproj/project.pbxproj"
 PLIST_FILE = "ios/Info.plist"
 
 def run():
-    configure_ios_package_name()
-    configure_ios_version()
-    configure_ios_orientation()
+    _configure_ios_package_name()
+    _configure_ios_version()
+    _configure_ios_orientation()
 
-def get_proj_dir():
+def _get_proj_dir():
     proj_dir = path("proj.ios_mac").realpath()
     if not proj_dir.exists():
         error("No ios project generated yet -- try cocowizard update")
     return proj_dir
 
-def configure_ios_orientation():
-    plist_file = get_proj_dir() / PLIST_FILE
-
-    text = plist_file.text()
-
+def _configure_ios_orientation():
     orientation = config.get("general.orientation")
-
     if orientation == "landscape":
         pass
     elif orientation == "portrait":
+        plist_file = _get_proj_dir() / PLIST_FILE
+        text = plist_file.text()
         text = text.replace("UIInterfaceOrientationLandscapeRight", "UIInterfaceOrientationPortrait")
         text = text.replace("UIInterfaceOrientationLandscapeLeft", "UIInterfaceOrientationPortraitUpsideDown")
+        plist_file.write_text(text)
     else:
         error("Orientation in configuration is invalid.")
 
-    plist_file.write_text(text)
-
-def configure_ios_package_name():
+def _configure_ios_package_name():
     project = config.get("general.project")
     package = config.get("general.package")
 
-    xcode_file = get_proj_dir() /  XCODE_FILE
-    
+    xcode_file = _get_proj_dir() /  XCODE_FILE
     text = xcode_file.text()
     text = text.replace("%s iOS" % project, project)
     xcode_file.write_text(text)
 
-    package_prefix = package.replace(project, "")
-
-    plist_file = get_proj_dir() / PLIST_FILE
+    plist_file = _get_proj_dir() / PLIST_FILE
     text = plist_file.text()
+    package_prefix = package.replace(project, "")
     text = text.replace("org.cocos2d-x.", package_prefix)
     plist_file.write_text(text)
 
-def configure_ios_version():
-    plist_file = get_proj_dir() / "ios/Info.plist"
+def _configure_ios_version():
+    plist_file = _get_proj_dir() / "ios/Info.plist"
     text = plist_file.text()
 
     if text.count("<string>1.0</string>") <> 1:
