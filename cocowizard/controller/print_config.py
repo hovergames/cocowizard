c9401eee2d5519741b1dbe32df92f5baf94a121a
Michael Contento <michaelcontento@gmail.com>
2013-10-30 13:32:55 +0100

    new command: print_config

---
+++ cocowizard/controller/print_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from ..utils import config
+from ..utils.log import indent, info
+
+def run():
+    _pprint(config.root())
+
+def _pprint(config):
+    for key, value in config.items():
+        if isinstance(value, dict):
+            info("%s:" % key)
+            with indent():
+                _pprint(value)
+        else:
+            info("%s: %s" % (key, value))
